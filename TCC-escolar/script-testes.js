22df1f59e400deb23189c1a4fc7cda822a06fc4d
Ryan de Souza Silva <107144128+RyanSS27@users.noreply.github.com>
2024-10-14 16:29:07 -0300

    navegação entre páginas

---
+++ TCC-escolar/script-testes.js
@@ -0,0 +1,91 @@
+/*var body = document.getElementsByTagName('body')
+body.addEventListener('scroll', rolou())
+
+let ultimaPosicao = 0
+
+rolou() {
+    if (scrollY > ultimaPosicao) {
+        var cabecalho = window.querySelector('header')
+        cabecalho.style.position = 'fixed'
+        ultimaPosicao = scrollY
+    } else {
+        var cabecalho = window.querySelector('header')
+        cabecalho.style.position = ''
+        ultimaPosicao = scrollY
+    }
+}*/
+
+
+//PASSAGEM DE PÁGINAS 
+
+var paginas = document.getElementsByClassName('pagina')
+var contPag = paginas.length
+var select = document.querySelector('select.select-paginas')
+var testes = document.getElementsByTagName('h1')[1]
+
+//Adiciona as páginas ao select
+for (cont = 1; cont<=contPag; cont++) {
+    var opt = document.createElement('option')
+    opt.setAttribute('value', `${cont-1}`)
+    opt.innerHTML = `${cont}/${contPag}`
+    select.appendChild(opt)
+}
+cont = ""
+
+
+//Some com as outras páginas
+var lendo = 0
+testes.innerHTML += lendo
+for (c = 0; c<=contPag; c++) {
+    paginas[c] != paginas[lendo]? paginas[c].style.display = "none" : paginas[c].style.display = 'block'
+}
+
+//Função que faz o navegar de páginas funcionar
+function trocaPag(avancarOUvoltar) {
+    if (avancarOUvoltar == 1 && lendo != 0) {
+        lendo = lendo - 1
+        testes.innerHTML += lendo
+        trocaSelect()
+    } else if (avancarOUvoltar == 2 && lendo + 1 < contPag) {
+        lendo = lendo + 1
+        testes.innerHTML += lendo
+        trocaSelect()
+    } else if (avancarOUvoltar == 0) {
+        lendo = select.value
+        testes.innerHTML += lendo
+    }
+    for (c = 0; c<contPag; c++) {
+        paginas[c] != paginas[lendo]? paginas[c].style.display = "none" : paginas[c].style.display = 'block'
+    }
+}
+
+
+function trocaSelect() {
+    select.innerHTML = ''
+    var opt = document.createElement('option')
+    opt.setAttribute('value', `${lendo}`)
+    opt.innerHTML = `${lendo+1}/${contPag}`
+    select.appendChild(opt)
+    for (cont = 1; cont<=contPag; cont++) {
+        if (cont - 1 != lendo) {
+            var opt = document.createElement('option')
+            opt.setAttribute('value', `${cont-1}`)
+            opt.innerHTML = `${cont}/${contPag}`
+            select.appendChild(opt)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TCC-escolar/script.js
+++ TCC-escolar/script.js
@@ -1,23 +1,51 @@
-/*var body = document.getElementsByTagName('body')
-body.addEventListener('scroll', rolou())
 
-let ultimaPosicao = 0
+var cap_interesse
+function ver_mais(cap) {
+    cap_interesse = cap
+}
 
-rolou() {
-    if (scrollY > ultimaPosicao) {
-        var cabecalho = window.querySelector('header')
-        cabecalho.style.position = 'fixed'
-        ultimaPosicao = scrollY
+var bloco = document.querySelector('div.bloco-topo')
+bloco.addEventListener(onload, verifica())
+
+function verifica() {
+    bloco.style.backgroundColor = 'blue'
+    bloco.innerHTML = `${cap_interesse}`
+    cap_interesse != null? abre_fecha(cap_interesse).cap_interesse = null: false 
+}
+
+
+function abre_fecha(cap, n) {
+    var part_inferior = document.getElementsByClassName('capitulos')[cap].querySelector('div.inferior-cap')
+    var parte_superior = document.getElementsByClassName('capitulos')[cap].querySelector('div.superior-cap')
+    var seta = parte_superior.querySelector('img.arrow-down')
+
+
+    if (part_inferior.style.display == 'block') {
+        part_inferior.style.display = 'none'
+        seta.style.transform = 'rotate(0deg)'
+        seta.style.transition = '0.5s'
     } else {
-        var cabecalho = window.querySelector('header')
-        cabecalho.style.position = ''
-        ultimaPosicao = scrollY
+        part_inferior.style.display = 'block'
+        seta.style.transform = 'rotate(180deg)'
+        seta.style.transition = '0.5s'
     }
-}*/
+}
 
 
-//PROGRESS BAR
+//PASSAGEM DE PÁGINA 
 
 
 
+
+
+
+
+
+
+// function teste() {
+//     var cap_tamanho = document.getElementsByClassName('capitulos')[0]
+//     var valor = Number(cap_tamanho.style.height)
+//     var div_ajustada = document.querySelector('div.div-broxa-coloridinha-do-gay-daniel')
+//     div_ajustada.style.minHeight = `${valor}px`
+// }
 
