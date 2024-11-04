81a3a2d2e6ab47f8cc2a9517c00684a82d639a5f
Ryan de Souza Silva <107144128+RyanSS27@users.noreply.github.com>
2024-11-04 12:15:47 -0300

    header amostra responsivo

--- header-usuario.php
+++ header-usuario.php
@@ -3,7 +3,7 @@
 ?>
 <header>
     <img src="imagens/logo-estudo-expresso-fundo-escuro.png" alt="Estudo & Expresso" class="logo">
-    <div>
+    <div class="container-nav">
         <nav>
             <a href="<?php echo $base_url; ?>/index.php">INICIO</a>
             
@@ -13,6 +13,15 @@
                 <button class="cadastro deslogar" onclick="logout()">Deslogar</button>
             </div>
         </nav>
+        <div class="menu-mobile">
+            <button><img src="icons/menu-hamburguer.png" alt="Menu" onclick="menuMobile()" class="icon-menu"></button>
+        </div>
+    </div>
+    <div class="nav-mobile">
+        <a href="<?php echo $base_url; ?>/index.php">INICIO</a>
+        <a href="<?php echo $base_url; ?>/quem-somos.php">QUEM SOMOS?</a>
+        <a href="<?php echo $base_url; ?>/meu-aprendizado.php">MEU APRENDIZADO</a>
+        <a href="<?php echo $base_url; ?>/formularios/login.html" class="login-mobile" onclick="logout()">Deslogar</a>
     </div>
 </header>
 
